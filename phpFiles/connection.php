f1c8b1527114f6936949b8e62cf6efae278f447d
Faraz <farazk093@gmail.com>
2019-10-20 06:59:22 -0700

    This is really hard

---
+++ phpFiles/connection.php
@@ -0,0 +1,8 @@
+<?php 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database="test";
+    //making connection with database
+    $conn = mysqli_connect($servername, $username, $password,$database);
+?>
